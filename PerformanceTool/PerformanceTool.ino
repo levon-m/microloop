ee2bcbe7f3f03f2253db31b25cbfe61bda6d8595
Levon Melkonyan <levon.melkonyan.cs@gmail.com>
2025-09-23 10:38:44 -0700

    Update PerformanceTool.ino

--- PerformanceTool/PerformanceTool.ino
+++ PerformanceTool/PerformanceTool.ino
@@ -1,34 +1,32 @@
 #include <Audio.h>
-#include <Wire.h>
-#include <SPI.h>
-#include <SD.h>
-#include <SerialFlash.h>
+#include <MIDI.h>
 #include "SGTL5000.h"
+#include "midi_io.h"
+#include "app_logic.h"
 
-AudioInputI2S            i2s1;
-AudioOutputI2S           i2s2;
-AudioConnection          patchCord1(i2s1, 0, i2s2, 0);
-AudioConnection          patchCord2(i2s1, 1, i2s2, 1);
-SGTL5000                 codec;
+AudioInputI2S i2s1;
+AudioOutputI2S i2s2;
+AudioConnection patchCord1(i2s1, 0, i2s2, 0);
+AudioConnection patchCord2(i2s1, 1, i2s2, 1);
+SGTL5000 codec;
 
 void setup() {
-  // Audio connections require memory to work.  For more
-  // detailed information, see the MemoryAndCpuUsage example
+  Serial.begin(115200);
+
   AudioMemory(12);
-
-  // Enable the audio shield
   codec.enable();
-}
 
-//elapsedMillis volmsec=0;
+  // DIN MIDI on Serial8 (RX8=34, TX8=35)
+  MidiIO::beginDIN8();
+  AppLogic::begin();
+
+  // Start threads
+  threads.addThread([]{ MidiIO::threadLoop(); }, 2048);   // I/O thread
+  threads.addThread([]{ AppLogic::threadLoop(); }, 3072); // App/UI thread
+
+  Serial.println("TeensyThreads: MIDI I/O + App threads started.");
+}
 
 void loop() {
-  // every 50 ms, adjust the volume
-  //if (volmsec > 50) {
-  //  float vol = analogRead(15);
-  //  vol = vol / 1023.0;
-  //  //audioShield.volume(vol); // <-- uncomment if you have the optional
-  //  volmsec = 0;               //     volume pot on your audio shield
-  //}
+  // empty; all work in threads (Audio runs in its own ISR)
 }
-
