d6484775fe8945371e35c56dbd513dd7b2587773
Levon Melkonyan <levon.melkonyan.cs@gmail.com>
2025-09-23 17:00:54 -0700

    Update PerformanceTool.ino

--- PerformanceTool/PerformanceTool.ino
+++ PerformanceTool/PerformanceTool.ino
@@ -17,12 +17,20 @@ void setup() {
   codec.enable();
 
   // DIN MIDI on Serial8 (RX8=34, TX8=35)
-  MidiIO::beginDIN8();
+  MidiIO::begin();
   AppLogic::begin();
 
   // Start threads
-  threads.addThread([]{ MidiIO::threadLoop(); }, 2048);   // I/O thread
-  threads.addThread([]{ AppLogic::threadLoop(); }, 3072); // App/UI thread
+  int ioId = threads.addThread([]{ MidiIO::threadLoop(); }, 2048);   // I/O thread
+  int appId = threads.addThread([]{ AppLogic::threadLoop(); }, 3072); // App/UI thread
+
+  //make io super responsive but polite, app gets a bit more contiguous time
+  //smaller slices improve responsiveness at the cost of more context switches
+  //currently app work is very light, smaller io slice gets more frequent turns
+  //moderate slice for app so it can finish a display update without being chopped too often
+  //reach for this kind of thing if UI work occasionally takes longer, like with big graphic/SD card writes, etc. and notice MIDI jitter or missed ticks. (could I measure that somehow?)
+  //threads.setTimeSlice(ioId, 2);
+  //threads.setTimeSlice(appId, 5);
 
   Serial.println("TeensyThreads: MIDI I/O + App threads started.");
 }
